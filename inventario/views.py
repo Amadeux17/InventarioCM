5f89c1f814a6df521ba63d1ba4f57ad8e3360b04
Amadeux17 <alfredoloyocarias02@gmail.com>
2023-07-16 00:18:39 -0600

    index

--- inventario/views.py
+++ inventario/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
-    return HttpResponse("Hola Mundo")
+    return render(request, "index.html")
+
+def ingresar(request):
+    return render(request, "inventario/ingresar.html")

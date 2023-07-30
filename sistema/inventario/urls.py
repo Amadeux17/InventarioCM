8607981a8e85831b116ba09f829a0bd929809cff
Amadeux17 <alfredoloyocarias02@gmail.com>
2023-07-29 20:56:38 -0600

    sistema

---
+++ sistema/inventario/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.inicio, name="inicio"),
+    path('productos', views.productos, name="productos"),
+    path('categorias', views.categorias, name="categorias")
+]
